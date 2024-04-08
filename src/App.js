54e38b2f56f2c676288280d222fc386185bfbb5e
Amanda Ventura <74805696+ASV185@users.noreply.github.com>
2024-04-08 11:59:08 -0600

    added h1 to weather component, and changed organization and tags of weather component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import WeatherSearch from "./WeatherSearch";
 import Weather from "./Weather";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Weather App</h1>
-        <WeatherSearch />
-        <Weather />
-      </header>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://amandaventura.netlify.app/">Amanda Ventura</a> and is{" "}
-        <a href="https://github.com/ASV185/weather-react-app">
-          open-sourced on Github
-        </a>
-      </footer>
+      <div classsName="container">
+        <header className="App-header">
+          <Weather />
+        </header>
+        <footer>
+          This project was coded by{" "}
+          <a href="https://amandaventura.netlify.app/">Amanda Ventura</a> and is{" "}
+          <a href="https://github.com/ASV185/weather-react-app">
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -2,5 +2,34 @@ import React from "react";
 import "./Weather.css";
 
 export default function Weather() {
-  return <div className="Weather"> Hello from weather</div>;
+  return (
+    <div className="Weather">
+      <div className="row">
+        <div className="col-4">
+          <input
+            type="search"
+            placeholder="Enter a city..."
+            className="form-control"
+          />
+        </div>
+        <div className="col-2">
+          <input type="submit" value="Search" className="btn btn-primary" />
+        </div>
+        <div className="col-2">Forecast</div>
+        <div className="col-2">My current location</div>
+      </div>
+      <h1>El Paso</h1>
+      <div className="row">
+        <div className="col-4">18 F | C</div>
+        <div className="col-4">Icon</div>
+      </div>
+      <div className="row">
+        <ul>
+          <li>Description:</li>
+          <li>Humidity:</li>
+          <li>Wind:</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
